08b26691c3d9a7558629b836967036d2d7b3c187
yogesh-wai15jay <121447264+yogesh-wai15jay@users.noreply.github.com>
2026-05-22 09:34:59 +0530

    Finalized Project email config changes

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -13,6 +13,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 const sendEmail = async (to, subject, html, attachments = []) => {
