a29a2843a190b121659db5fab54bd79986f82e85
yogesh-wai15jay <121447264+yogesh-wai15jay@users.noreply.github.com>
2026-05-22 09:47:57 +0530

    Finalized Project with revert email config changes

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -8,7 +8,7 @@ const path = require('path');
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: parseInt(process.env.SMTP_PORT),
-  secure: process.env.SMTP_SECURE === 'true',
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
